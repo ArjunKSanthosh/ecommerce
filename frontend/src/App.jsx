6e6d8f48a39bac589a5e59ce811478fe94b5da10
ArjunkSanthosh <arjunk80043@gmail.com>
2024-12-25 15:23:50 +0530

    kazhinju

---
+++ frontend/src/App.jsx
@@ -0,0 +1,18 @@
+import React,{useState} from 'react'
+import {BrowserRouter,Routes,Route} from 'react-router-dom'
+import Home from './Components/Home'
+import './App.css'
+
+function App() {
+
+
+  return (
+    <BrowserRouter>
+        <Routes>
+            <Route path='/' Component={Home}/>
+        </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App
---
+++ frontend/src/Components/Home.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+function Home(){
+    return(
+        <>
+            <div className="main">
+                <div className="container">
+                    <h2>Ecommerce</h2>
+                </div>
+            </div>
+        </>
+    )
+}
+export default Home;
